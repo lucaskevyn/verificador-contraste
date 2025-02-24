d60f2c3fb43dfaf5b3c3ec062ad2bfe484ba1dc1
Lucas Kevin <lucas.castro@tjro.jus.br>
2025-02-24 19:23:00 -0400

    style: focus do input de cor

--- app/components/Seletor.tsx
+++ app/components/Seletor.tsx
@@ -9,9 +9,9 @@ export default function Seletor({ titulo, nome, cor, onChange }: SeletorProps) {
   return (
     <div className="">
       {titulo}
-      <div className='flex w-64 h-11 border-2 rounded-md'>
-        <input type='text' name={nome} className='h-full w-full pl-3' onChange={onChange} value={cor} />
-        <input type='color' name={nome} className='h-full w-12 cursor-pointer border-l-2' onChange={onChange} value={cor} />
+      <div className='group flex w-64 h-11 border border-gray-300 rounded-lg focus-within:border-lime-600 hover:border-lime-600'>
+        <input type='text' name={nome} className='h-full w-full pl-3 rounded-lg focus:outline-none' onChange={onChange} value={cor} />
+        <input type='color' name={nome} className='h-full w-12 cursor-pointer border-l-2 rounded-r-lg' onChange={onChange} value={cor} />
       </div>
     </div>
   );
