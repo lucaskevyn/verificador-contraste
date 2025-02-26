a34a5119576342e0da0183f1665a93f7efecc41d
Lucas Kevin <lucas.castro@tjro.jus.br>
2025-02-26 11:27:26 -0400

    fix: build erro

--- app/components/Seletor.tsx
+++ app/components/Seletor.tsx
@@ -1,20 +1,40 @@
 interface SeletorProps {
-  titulo: string
-  nome: string
-  cor: string
-  lum: number
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  titulo: string;
+  nome: string;
+  cor: string;
+  lum: number;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function Seletor({ titulo, nome, cor, lum, onChange }: SeletorProps) {
+export default function Seletor({
+  titulo,
+  nome,
+  cor,
+  lum,
+  onChange,
+}: SeletorProps) {
   return (
     <div className="flex flex-col gap-2">
       <span className="font-bold">{titulo}</span>
-      <div className='group flex h-11 border border-gray-300 rounded-lg focus-within:border-lime-600 hover:border-lime-600'>
-        <input type='text' name={nome} className='h-full w-full pl-3 rounded-l-lg focus:outline-none font-medium' onChange={onChange} value={cor} />
-        <input type='color' name={nome} className='h-full w-12 cursor-pointer border-l-2 rounded-r-lg' onChange={onChange} value={cor} />
+      <div className="group flex h-11 border border-gray-300 rounded-lg focus-within:border-azulTexto hover:border-azulClaroTexto">
+        <input
+          type="text"
+          name={nome}
+          className="h-full w-full pl-3 rounded-l-lg focus:outline-none font-medium"
+          onChange={onChange}
+          value={cor}
+        />
+        <input
+          type="color"
+          name={nome}
+          className="h-full w-12 cursor-pointer border-l-2 rounded-r-lg"
+          onChange={onChange}
+          value={cor}
+        />
       </div>
-      <div className="font-medium">Luminância: {Math.round(lum * 100) / 100}</div>
+      <div className="font-medium">
+        Luminância: {Math.round(lum * 100) / 100}
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,84 +1,147 @@
-'use client'
+"use client";
 import Seletor from "./components/Seletor";
 import { useState } from "react";
-import { Star } from 'lucide-react';
+import { Star } from "lucide-react";
 
 export default function Home() {
   const [cores, setCores] = useState({
-    texto: '#ffffff',
-    fundo: '#375081'
-  })
+    texto: "#ffffff",
+    fundo: "#375081",
+  });
 
-  const rgbTexto = hexParaRgb(cores.texto)
-  const rgbFundo = hexParaRgb(cores.fundo)
+  const rgbTexto = hexParaRgb(cores.texto);
+  const rgbFundo = hexParaRgb(cores.fundo);
 
-  const lumTexto = luminancia(rgbTexto)
-  const lumFundo = luminancia(rgbFundo)
+  const lumTexto = luminancia(rgbTexto);
+  const lumFundo = luminancia(rgbFundo);
 
-  const contraste = lumTexto > lumFundo
-    ? ((lumTexto + 0.05) / (lumFundo + 0.05))
-    : ((lumFundo + 0.05) / (lumTexto + 0.05))
+  const contraste =
+    lumTexto > lumFundo
+      ? (lumTexto + 0.05) / (lumFundo + 0.05)
+      : (lumFundo + 0.05) / (lumTexto + 0.05);
 
-  const nivel = contraste > 7 ? 3
-    : contraste > 4.5 ? 2
-      : contraste > 3 ? 1
-        : 0
+  const nivel = contraste > 7 ? 3 : contraste > 4.5 ? 2 : contraste > 3 ? 1 : 0;
 
   function mudarCor(e: React.ChangeEvent<HTMLInputElement>) {
-    const { name, value } = e.target
-    setCores((prev) => ({ ...prev, [name]: value }))
+    const { name, value } = e.target;
+    setCores((prev) => ({ ...prev, [name]: value }));
   }
 
   function hexParaRgb(hex: string) {
-    var regex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i
-    hex = hex.replace(regex, function (m, r, g, b) { return r + r + g + g + b + b })
+    const regex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(regex, function (m, r, g, b) {
+      return r + r + g + g + b + b;
+    });
 
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-    return result ? {
-      r: parseInt(result[1], 16),
-      g: parseInt(result[2], 16),
-      b: parseInt(result[3], 16)
-    } : { r: 0, g: 0, b: 0 }
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result
+      ? {
+          r: parseInt(result[1], 16),
+          g: parseInt(result[2], 16),
+          b: parseInt(result[3], 16),
+        }
+      : { r: 0, g: 0, b: 0 };
   }
 
-  function luminancia(rgb: { r: number, g: number, b: number }) {
-    var a = [rgb.r, rgb.g, rgb.b].map(function (v) {
-      v /= 255
-      return v <= 0.04045 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4)
-    })
-    return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722
+  function luminancia(rgb: { r: number; g: number; b: number }) {
+    const a = [rgb.r, rgb.g, rgb.b].map(function (v) {
+      v /= 255;
+      return v <= 0.04045 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
+    });
+    return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
   }
 
   return (
-    <main className='w-full h-screen justify-center items-center flex flex-col'>
+    <main className="w-full h-screen justify-center items-center flex flex-col">
       <div className="flex flex-col w-3/5 gap-6 font-bold">
-        <h1 className="w-full text-6xl flex flex-col text-azulTexto">VERIFICADOR<br /> <span className="text-5xl font-normal tracking-tight text-azulClaroTexto">DE CONTRASTE</span></h1>
+        <h1 className="w-full text-6xl flex flex-col text-azulTexto">
+          VERIFICADOR
+          <br />{" "}
+          <span className="text-5xl font-normal tracking-tight text-azulClaroTexto">
+            DE CONTRASTE
+          </span>
+        </h1>
         <div className="flex rounded-2xl border">
           <div className="flex flex-col w-1/2 p-6 gap-8">
             <div className="flex gap-4">
-              <Seletor titulo='Cor do Texto' nome='texto' cor={cores.texto} lum={lumTexto} onChange={mudarCor} />
-              <Seletor titulo='Cor do Fundo' nome='fundo' cor={cores.fundo} lum={lumFundo} onChange={mudarCor} />
+              <Seletor
+                titulo="Cor do Texto"
+                nome="texto"
+                cor={cores.texto}
+                lum={lumTexto}
+                onChange={mudarCor}
+              />
+              <Seletor
+                titulo="Cor do Fundo"
+                nome="fundo"
+                cor={cores.fundo}
+                lum={lumFundo}
+                onChange={mudarCor}
+              />
             </div>
             <div className="flex flex-col gap-2">
               <span className="">Contraste</span>
               <div className="flex flex-col h-64 gap-1  text-lg">
-                <div className={`flex h-3/5 rounded-t-2xl p-8 justify-between items-center ${nivel === 3 ? "bg-verde text-verdeTexto" : nivel === 2 ? "bg-amarelo text-amareloTexto" : "bg-vermelho text-vermelhoTexto"}`}>
-                  <span className="text-6xl"> {Math.round(contraste * 100) / 100} </span>
+                <div
+                  className={`flex h-3/5 rounded-t-2xl p-8 justify-between items-center ${
+                    nivel === 3
+                      ? "bg-verde text-verdeTexto"
+                      : nivel === 2
+                      ? "bg-amarelo text-amareloTexto"
+                      : "bg-vermelho text-vermelhoTexto"
+                  }`}
+                >
+                  <span className="text-6xl">
+                    {" "}
+                    {Math.round(contraste * 100) / 100}{" "}
+                  </span>
                   <div className="flex flex-col items-center gap-1">
-                    <span>{nivel === 3 ? "Ótimo" : nivel === 2 ? "Bom" : nivel === 1 ? "Ruim" : "Muito Ruim"}</span>
+                    <span>
+                      {nivel === 3
+                        ? "Ótimo"
+                        : nivel === 2
+                        ? "Bom"
+                        : nivel === 1
+                        ? "Ruim"
+                        : "Muito Ruim"}
+                    </span>
                     <div className="flex gap-1">
-                      <Star className={nivel > 0 ? "fill-current" : undefined}></Star>
-                      <Star className={nivel > 1 ? "fill-current" : undefined}></Star>
-                      <Star className={nivel > 2 ? "fill-current" : undefined}></Star>
+                      <Star
+                        className={nivel > 0 ? "fill-current" : undefined}
+                      ></Star>
+                      <Star
+                        className={nivel > 1 ? "fill-current" : undefined}
+                      ></Star>
+                      <Star
+                        className={nivel > 2 ? "fill-current" : undefined}
+                      ></Star>
                     </div>
                   </div>
                 </div>
                 <div className="flex h-2/5 gap-1">
-                  <div className={`flex w-1/2 rounded-bl-2xl p-8 justify-between items-center ${nivel === 3 ? "bg-verde text-verdeTexto" : nivel === 2 ? "bg-amarelo text-amareloTexto" : "bg-vermelho text-vermelhoTexto"}`}>
+                  <div
+                    className={`flex w-1/2 rounded-bl-2xl p-8 justify-between items-center ${
+                      nivel === 3
+                        ? "bg-verde text-verdeTexto"
+                        : nivel === 2
+                        ? "bg-amarelo text-amareloTexto"
+                        : "bg-vermelho text-vermelhoTexto"
+                    }`}
+                  >
                     Texto Pequeno
-                    <span className="">{nivel == 3 ? "AAA" : nivel === 2 ? "AA" : ""}</span>
+                    <span className="">
+                      {nivel == 3 ? "AAA" : nivel === 2 ? "AA" : ""}
+                    </span>
                   </div>
-                  <div className={`flex w-1/2 rounded-br-2xl p-8 justify-between items-center ${nivel > 1 ? "bg-verde text-verdeTexto" : nivel === 1 ? "bg-amarelo text-amareloTexto" : "bg-vermelho text-vermelhoTexto"}`}>
+                  <div
+                    className={`flex w-1/2 rounded-br-2xl p-8 justify-between items-center ${
+                      nivel > 1
+                        ? "bg-verde text-verdeTexto"
+                        : nivel === 1
+                        ? "bg-amarelo text-amareloTexto"
+                        : "bg-vermelho text-vermelhoTexto"
+                    }`}
+                  >
                     Texto Grande
                     <span>{nivel > 1 ? "AAA" : nivel === 1 ? "AA" : ""}</span>
                   </div>
@@ -86,9 +149,18 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className={`flex flex-col justify-center w-1/2 rounded-r-2xl p-12 gap-2`} style={{ backgroundColor: cores.fundo, color: cores.texto }}>
+          <div
+            className={`flex flex-col justify-center w-1/2 rounded-r-2xl p-12 gap-2`}
+            style={{ backgroundColor: cores.fundo, color: cores.texto }}
+          >
             <h2 className=" text-2xl">Exemplo</h2>
-            <p className="text-lg font-normal">As you type, the contrast ratio indicated will update. Hover over the circle to get more detailed information. When semi-transparent colors are involved as backgrounds, the contrast ratio will have an error margin, to account for the different colors they may be over.</p>
+            <p className="text-lg font-normal">
+              As you type, the contrast ratio indicated will update. Hover over
+              the circle to get more detailed information. When semi-transparent
+              colors are involved as backgrounds, the contrast ratio will have
+              an error margin, to account for the different colors they may be
+              over.
+            </p>
           </div>
         </div>
       </div>
